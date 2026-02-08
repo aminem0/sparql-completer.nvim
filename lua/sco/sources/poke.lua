ff29f607386364d645ecde3ccee9657ece315b00
El-Amine Mimouni <el-amine.mimouni@mcgill.ca>
2026-02-08 18:00:19 -0500

    add nerc sparql endpoint (with a couple of poke: datatype properties)

--- lua/sco/sources/poke.lua
+++ lua/sco/sources/poke.lua
@@ -37,4 +37,53 @@ return {
 ]],
         },
     },
+    {
+        label = "poke:color",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "poke:color",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [https://triplydb.com/academy/pokemon/vocab/color]
+
++ **Type**
+- [owl:DatatypeProperty]
+
++ **Domain**
+- [poke:Pokémon]
+
++ **Range**
+- [xsd:string]
+
++ **Definition**
+- The color of a Pokémon.
+]],
+        },
+    },
+    {
+        label = "poke:stepsToHatch",
+        kind = cmp.lsp.CompletionItemKind.Property,
+        description = "poke:stepsToHatch",
+        documentation = {
+            kind = "markdown",
+            value = [[
++ **Identifier**
+- [https://triplydb.com/academy/pokemon/vocab/stepsToHatch]
+
++ **Type**
+- [owl:DatatypeProperty]
+
++ **Domain**
+- [poke:Pokémon]
+
++ **Range**
+- [xsd:integer]
+
++ **Definition**
+- The numer of steps required for an egg of this Pokémon to hatch.
+]],
+        },
+    },
+
 }
