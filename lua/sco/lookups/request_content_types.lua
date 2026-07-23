3797f6b750448947c573366c566739e4519fad03
El-Amine Mimouni <el-amine.mimouni@mcgill.ca>
2026-07-23 19:41:26 -0400

    add application/json to content type lookup table. not functional for now

--- lua/sco/lookups/request_content_types.lua
+++ lua/sco/lookups/request_content_types.lua
@@ -1,4 +1,5 @@
 return {
+    { name = "Form",  mime_type = "application/x-www-form-urlencoded" },
+    { name = "JSON",  mime_type = "application/json" },
     { name = "Query", mime_type = "application/sparql-query" },
-    { name = "Form (you heathen!)", mime_type = "application/x-www-form-urlencoded" },
 }
